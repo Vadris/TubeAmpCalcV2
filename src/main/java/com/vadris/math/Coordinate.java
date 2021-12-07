fa2c14c58c95c47497ff3879ae16fae1f59c5cf6
Vadris <72710015+Vadris@users.noreply.github.com>
2021-12-07 11:41:45 +0100

    Create Coordinate.java

---
+++ src/main/java/com/vadris/math/Coordinate.java
@@ -0,0 +1,39 @@
+package com.vadris.math;
+
+public class Coordinate {
+    private double x;
+    private double y;
+    private String name;
+
+    public Coordinate(String name,double x,double y){
+        this.name = name;
+        this.x = x;
+        this.y = y;
+    }
+    public Coordinate(double x,double y){
+        name  = "P";
+        this.x = x;
+        this.y = y;
+    }
+
+    /** Getter Methods **/
+    public double getX(){
+        return x;
+    }
+    public double getY() {
+        return y;
+    }
+    public String getName() {
+        return name;
+    }
+    /** Setter Methods **/
+    public void setX(double x) {
+        this.x = x;
+    }
+    public void setY(double y) {
+        this.y = y;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
