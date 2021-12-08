bc931a82c2eeca8e9a6b5a59a4846a7f30b7f17e
Vadris <72710015+Vadris@users.noreply.github.com>
2021-12-08 20:23:54 +0100

    Create Graph.java

---
+++ src/main/java/com/vadris/math/Graph.java
@@ -0,0 +1,56 @@
+package com.vadris.math;
+
+import java.util.ArrayList;
+
+public class Graph {
+    private String name;
+    private ArrayList<Coordinate> coordinates;
+
+    public Graph(){
+        name = "Graph";
+        coordinates = new ArrayList<>();
+    }
+    public Graph(String name){
+        this.name = name;
+        coordinates = new ArrayList<>();
+    }
+    public Graph(ArrayList<Coordinate> coordinates){
+        name = "Graph";
+        this.coordinates = coordinates;
+    }
+    public Graph(String name, ArrayList<Coordinate> coordinates){
+        this.name = name;
+        this.coordinates = coordinates;
+    }
+
+    public void addCoordinate(Coordinate coordinate){
+        coordinates.add(coordinate);
+    }
+    public void addCoordinate(int index, Coordinate coordinate){
+        coordinates.add(index, coordinate);
+    }
+
+    public void removeCoordinate(int index){
+        coordinates.remove(index);
+    }
+
+    @Override
+    public String toString() {
+        return "Graph{" +
+                "name='" + name + '\'' +
+                ", coordinates=" + coordinates +
+                '}';
+    }
+
+    /** Getter Methods **/
+    public String getName() {
+        return name;
+    }
+    public ArrayList<Coordinate> getCoordinates() {
+        return coordinates;
+    }
+    /** Setter Methods **/
+    public void setName(String name) {
+        this.name = name;
+    }
+}
