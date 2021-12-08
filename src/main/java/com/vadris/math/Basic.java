01570ce50888b2619073165508304da3ac0e0b51
Vadris <72710015+Vadris@users.noreply.github.com>
2021-12-08 21:43:47 +0100

    General Cleanup

--- src/main/java/com/vadris/math/Basic.java
+++ src/main/java/com/vadris/math/Basic.java
@@ -2,7 +2,7 @@ package com.vadris.math;
 /** Basic math functions **/
 public final class Basic {
     /** Gets the nth Root of x **/
-    public static double nthRootOF(double n, double x) throws Exception {
+    public static double nthRootOf(double n, double x) throws Exception {
         if(n % 2 != 0 && x < 0){
             throw new Exception("Can not get even root of a negative number");
         }
@@ -12,7 +12,13 @@ public final class Basic {
         return Math.pow(x, (1/n));
     }
 
-    /** returns true if x falls in between the boundaries **/
+    /**
+     * Returns true if x falls in between the boundaries
+     * @param x value to be tested
+     * @param boundA First bound
+     * @param boundB Second bound
+     * @return <code>true</code> if x falls in between the boundaries
+     */
     public static boolean isInRange(double x, double boundA, double boundB){
         if(boundB > boundA){
             return x < boundB && x > boundA;
