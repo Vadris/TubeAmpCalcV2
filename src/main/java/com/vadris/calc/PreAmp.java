607f8e3fdb684dbf53c23e3a2841c4a35c6270a3
Vadris <72710015+Vadris@users.noreply.github.com>
2021-12-10 09:07:09 +0100

    Created Methods

--- src/main/java/com/vadris/calc/PreAmp.java
+++ src/main/java/com/vadris/calc/PreAmp.java
@@ -1,5 +1,9 @@
 package com.vadris.calc;
 
+import com.vadris.math.Coordinate;
+import com.vadris.math.Graph;
+import com.vadris.tube.Tube;
+
 /**
  * Implements calculations for tube preamps
  *
@@ -7,4 +11,19 @@ package com.vadris.calc;
  */
 public class PreAmp {
 
+    public static Graph calcLoadLine(double rLoad, double uAnode){
+        return null;
+    }
+
+    public static Coordinate calcOperatingPoint(Tube tube, Graph loadLine){
+        return  null;
+    }
+
+    public static double calcEqAnodeResistance(){
+        return 0;
+    }
+
+    public static double calcGain(Tube tube, Coordinate operatingPoint, double rLoad, double rBias, double rEqAnode){
+        return 0;
+    }
 }
